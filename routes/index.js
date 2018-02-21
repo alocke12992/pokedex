c200a26e2728063f4e8f25bf47bdb90d6666fdb9
Andrew Locke <alocke12992@gmail.com>
2018-02-21 09:36:44 -0700

    step back

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dexter' });
+  res.render('index', { title: 'https://fontmeme.com/permalink/180221/107c7feb0395c97d7f094e2a94ccb4b2.png' });
 });
 
 module.exports = router;
